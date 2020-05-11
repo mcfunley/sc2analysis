5b815e52d43152ab6cc1e9cf6cf6c89f3ab2d299
Dan McKinley <mcfunley@gmail.com>
2020-05-10 17:34:01 -0700

    use sc2reader

--- parse.py
+++ parse.py
@@ -1,7 +1,21 @@
 import os
 from pathlib import Path
-from zephyrus_sc2_parser import parse_replay
+import sc2reader
 
 sc2dir = os.path.expanduser(
     '~/Library/Application Support/Blizzard/Starcraft II')
 replays = Path(sc2dir).rglob('*.SC2Replay')
+
+def parse(fn):
+    players, timeline, summary_stats, metadata = parse_replay(
+        fn, local=False, detailed=True)
+    return {
+        'players': players,
+        'timeline': timeline,
+        'summary_stats': summary_stats,
+        'metadata': metadata,
+    }
+
+def parseone():
+    for p in replays:
+        return sc2reader.load_replay(str(p), load_level=4)
