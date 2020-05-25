6f44b24f8ccc0fbd1acfd23a0282f658ea6ac994
Dan McKinley <mcfunley@gmail.com>
2020-05-25 15:25:34 -0700

    some views

---
+++ migrations/V2020_05_25_22_16_31__mcfunley-games.sql
@@ -0,0 +1,7 @@
+-- -*- sql-dialect: postgres; -*-
+create view ladder_1v1 as
+select * from games where is_ladder and type='1v1';
+
+
+create view mcfunley as
+select player_id, game_id from players where name='mcfunley';
